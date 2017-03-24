7eb07b4df92e2ceae555413637e5fee163b04972
JIY60 <jiy60@pitt.edu>
2017-03-24 13:51:53 -0400

    add functions

--- Function/functions.R
+++ Function/functions.R
@@ -1,5 +1,7 @@
-dat<-read.table("ShortenClientsMerged")
+dat<-read.table("ShortenClientsMerged.txt")
+
 # Number of people with mental health in each family
+# Number of people in the family
 lengthnoNA<-function(x){
   length(x[-which(is.na(x))])
 }
@@ -7,6 +9,10 @@ AggregateByCase<-function(group,x){
   output<-tapply(x,group,lengthnoNA)
   data.frame(output)
 }
-# Close times. 
 
-# Number of people in the 
+# Close times. 
+install.packages("stringi")
+library("stringi")
+countclosedate<-function(closedate){
+  output<-str_count(x,",")
+}
