7477c37f6591b528124f99f69073dcef8d2d8977
alberto-guzman <alvarez1991@gmail.com>
2017-03-29 13:25:23 -0400

    Added a ggplot code for our specific project, outside the function.

--- Graphing Functions-Alberto .R
+++ Graphing Functions-Alberto .R
@@ -20,4 +20,13 @@ ScatterDotSize(dat$nMH,dat$nClose,dat$nClients)
 
 
 
+#use this code for our specific graph, uses labels 
 
+library(ggplot2)
+ggplot(aes(dat$nMH,dat$nClose), data = dat) +
+  geom_point(aes(size=dat$nClients)) +    # Use hollow circles
+  geom_smooth(method=lm) +  # Add linear regression line 
+  geom_jitter() +
+  xlab("Number of MH Services") +
+  ylab("Number of Closed Cases") +
+  ggtitle("Number of DHS Mental Health Services by Number of Closed Case Dates")
